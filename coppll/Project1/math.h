03f7391ca083b4b2f98f29e158e8c676cf6fa7eb
VNSaphonova23 <VNSaphonova23@codingburgas.bg>
2026-01-22 14:48:05 +0200

    Add files via upload

---
+++ coppll/Project1/math.h
@@ -0,0 +1,7 @@
+#pragma once
+int addition(int a, int b);
+int subtract(int a, int b);
+
+int multiply(int a, int b);
+
+int divide(int a, int b);
---
+++ coppll/coppll/coppll.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+#include"../Project1/math.h"
+using namespace std;
+int main() {
+    int a, b;
+    char znak;
+
+    cout << "enter : ";
+    cin >> a >> znak >> b;
+
+    int rezultat = 0;
+
+    if (znak == '+') {
+        rezultat = addition(a, b);
+    }
+    if (znak == '-') {
+        rezultat = subtract(a, b);
+    }
+    if (znak == '*') {
+        rezultat = multiply(a, b);
+    }
+    if (znak == '/') {
+        rezultat = divide(a, b);
+    }
+
+    cout << "result : " << rezultat << endl;
+
+
+}
+
